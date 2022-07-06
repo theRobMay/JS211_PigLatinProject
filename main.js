2f38b87d26cad6a30fa4fb506f2536a3b7038ac9
theRobMay <robbie.maysent@gmail.com>
2022-07-05 20:05:16 -0500

    test env done

--- main.js
+++ main.js
@@ -11,10 +11,38 @@ const rl = readline.createInterface({
 });
 
 
-const pigLatin = (word) => {
+const pigLatin = (answer) => {
+  // break your code into pieces and focus on one piece at a time...
+  // 1. if word begins with a vowel send to one function: adds "yay"
+  // 2. if word begins with a consonant send to another function: splices off beginning, returns word with new ending.
+  // 3. if multiple words, create array of words, loop over them, sending them to different functions and creating a new array with the new words.
+  //find first vowel in word
+  //if the first vowel is first letter in string add 'yay' to the end of word
+  //if first letter of string is a consonant followed by a vowel move the first letter to end of string then add 'yay'
+  //if the strings first 2 letters are consonants move both to the end and add 'yay'
+  let vowels = ['a', 'e', 'i', 'o', 'u'];
+  let cleanWord = answer.trim().toLowerCase()
+  let firstLetter = cleanWord.charAt(0)
+  let complexTest = cleanWord.slice(0,2)
+  let complexSplit = complexTest.slice(1)
 
-  // Your code here
-
+  if (vowels.includes(firstLetter)) {
+    let vowelFirst = cleanWord + 'yay'
+    console.log(`${cleanWord} is a vowel`)
+    return vowelFirst
+  }
+  else {
+    if (vowels.includes(complexSplit)) {
+      let vowelSecondFirst = cleanWord.slice(1)
+      let vowelSecondSecond = cleanWord.slice(0,1)
+      return vowelSecondFirst + vowelSecondSecond + 'ay'
+    }
+    else {
+      let noVowelFirst = cleanWord.slice(2)
+      let noVowelSecond = cleanWord.slice(0,2)
+      return noVowelFirst + noVowelSecond + 'ay'
+    }
+  }
 }
 
 // the first function called in the program to get an input from the user
